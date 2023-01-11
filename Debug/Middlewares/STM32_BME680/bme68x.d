a41f993ff4c6a90a7b38df8c9b657b4c6620a547
Nathan Perry <np@nathanperry.dev>
2023-01-11 04:02:55 -0500

    repo: remove binaries and object files

--- Debug/Middlewares/STM32_BME680/bme68x.d
+++
@@ -1,7 +0,0 @@
-Middlewares/STM32_BME680/bme68x.o: ../Middlewares/STM32_BME680/bme68x.c \
- ../Middlewares/STM32_BME680/bme68x.h \
- ../Middlewares/STM32_BME680/bme68x_defs.h
-
-../Middlewares/STM32_BME680/bme68x.h:
-
-../Middlewares/STM32_BME680/bme68x_defs.h:
--- Debug/Middlewares/Sensirion_Core/src/SensirionCrc.d
+++
@@ -1,5 +0,0 @@
-Middlewares/Sensirion_Core/src/SensirionCrc.o: \
- ../Middlewares/Sensirion_Core/src/SensirionCrc.cpp \
- ../Middlewares/Sensirion_Core/src/../../Sensirion_Core/src/SensirionCrc.h
-
-../Middlewares/Sensirion_Core/src/../../Sensirion_Core/src/SensirionCrc.h:
