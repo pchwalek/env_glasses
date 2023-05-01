cbbbf36a9f54d3f0e878c7713c9b7adfcea8c69b
Patrick Chwalek <patrickchwalek@gmail.com>
2023-05-01 13:10:40 -0400

    changed frequency of speaker (blink modulator) to lower

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -46,7 +46,8 @@ void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 1599;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 19;
+//  htim2.Init.Period = 19;
+  htim2.Init.Period = 59;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
